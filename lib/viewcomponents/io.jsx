aecbe74c993aee31cd5ce77b310c58c483026f79
Julian Boilen <julianboilen@gmail.com>
2015-02-10 22:05:33 -0800

    Reindent

--- lib/viewcomponents/io.jsx
+++ lib/viewcomponents/io.jsx
@@ -5,73 +5,73 @@ var LabelPosition = require('../enum/ioLabelPosition');
 var ioRadius = 4;
 
 var IO = React.createClass({
-    propTypes: {
-        isInput: React.PropTypes.bool.isRequired,
+  propTypes: {
+    isInput: React.PropTypes.bool.isRequired,
 
-        label: React.PropTypes.string,
+    label: React.PropTypes.string,
 
-        x: React.PropTypes.number.isRequired,
+    x: React.PropTypes.number.isRequired,
 
-        y: React.PropTypes.number.isRequired,
+    y: React.PropTypes.number.isRequired,
 
-        size: React.PropTypes.number,
+    size: React.PropTypes.number,
 
-        labelPosition: React.PropTypes.oneOf(Object.keys(LabelPosition).map(function(k) {
-          return LabelPosition[k];
-        }))
-    },
-    /* it's the responsibility of the Node to position the IO since it's in its coordinate space */
-    render: function() {
-        var isInput = this.props.isInput;
+    labelPosition: React.PropTypes.oneOf(Object.keys(LabelPosition).map(function(k) {
+      return LabelPosition[k];
+    }))
+  },
+  /* it's the responsibility of the Node to position the IO since it's in its coordinate space */
+  render: function() {
+    var isInput = this.props.isInput;
 
-        var labelPosition = this._getLabelPositioningData(this.props.labelPosition, this.props.size || ioRadius);
+    var labelPosition = this._getLabelPositioningData(this.props.labelPosition, this.props.size || ioRadius);
 
-        var classes = React.addons.classSet({
-          'io': true,
-          'input': isInput,
-          'output': !isInput
-        });
+    var classes = React.addons.classSet({
+      'io': true,
+      'input': isInput,
+      'output': !isInput
+    });
 
-        return (
-           <g
-            className={classes}
-            transform={'translate(' + this.props.x + ', ' + this.props.y + ')'} >
-            <circle
-              r={this.props.size || ioRadius}
-              cx={0} cy={0} 
-              onMouseDown={this.props.onMouseDown}/>
-            <text class="label"
-                x={labelPosition.x}
-                y={labelPosition.y}
-                textAnchor={labelPosition.textAnchor} >
-                {this.props.label}
-            </text>
-            </g>
-        );
-    },
+    return (
+     <g
+       className={classes}
+       transform={'translate(' + this.props.x + ', ' + this.props.y + ')'} >
+       <circle
+         r={this.props.size || ioRadius}
+         cx={0} cy={0} 
+         onMouseDown={this.props.onMouseDown}/>
+       <text class="label"
+         x={labelPosition.x}
+         y={labelPosition.y}
+         textAnchor={labelPosition.textAnchor} >
+         {this.props.label}
+       </text>
+     </g>
+     );
+  },
 
-    _getLabelPositioningData: function(labelPosition, extraMargin) {
-      var offset = {x: 0, y: 0};
+  _getLabelPositioningData: function(labelPosition, extraMargin) {
+    var offset = {x: 0, y: 0};
 
-      var totalMargin = 4 + extraMargin;
+    var totalMargin = 4 + extraMargin;
 
-      switch (labelPosition) {
-        case LabelPosition.RIGHT:
-        return {x: totalMargin, y: 0, textAnchor: 'start'};
+    switch (labelPosition) {
+      case LabelPosition.RIGHT:
+      return {x: totalMargin, y: 0, textAnchor: 'start'};
 
-        case LabelPosition.LEFT:
-        return {x: -totalMargin, y: 0, textAnchor: 'end'};
+      case LabelPosition.LEFT:
+      return {x: -totalMargin, y: 0, textAnchor: 'end'};
 
-        case LabelPosition.ABOVE:
-        return {x: 0, y: -totalMargin, textAnchor: 'start'};
+      case LabelPosition.ABOVE:
+      return {x: 0, y: -totalMargin, textAnchor: 'start'};
 
-        case LabelPosition.BELOW:
-        return {x: 0, y: totalMargin, textAnchor: 'start'};
+      case LabelPosition.BELOW:
+      return {x: 0, y: totalMargin, textAnchor: 'start'};
 
-        default:
-        return {x: 0, y: 0, textAnchor: 'start'};
-      }
+      default:
+      return {x: 0, y: 0, textAnchor: 'start'};
     }
+  }
 
 });
 
