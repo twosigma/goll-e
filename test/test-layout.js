bcc090ec9e5c9e7fbc84ddc074be96357061fc86
Julian Boilen <julianboilen@gmail.com>
2015-02-14 18:34:50 -0500

    Add a super basic "test" for layout engine.
    It just gives it a graph and ensures there were no exceptions.

---
+++ test/test-layout.js
@@ -0,0 +1,26 @@
+var should = require('should');
+
+var lib = './../lib/';
+
+var Graph = require(lib + 'model/graph');
+var Node = require(lib + 'model/node');
+var Port = require(lib + 'model/port');
+var CardinalPortPosition = require(lib + 'model/cardinalPortPosition');
+
+var LayoutEngine = require(lib + 'layout/dummyLayoutV2');
+
+var generateGraph = function() {
+    // the world's simplest graph
+    var port = new Port('a+a', 'input', new CardinalPortPosition(50, 'N'));
+    var node = new Node('a', [port], []);
+    var graph = new Graph([node], []);
+    return graph;
+};
+
+
+describe('layout engine', function() {
+    it('should take a graph and not blow up', function(done) {
+      LayoutEngine(generateGraph());
+      done();
+    });
+});
