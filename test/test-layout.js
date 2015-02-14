6eee228e014cae89c88fc0100c3b697a1c195bcf
Julian Boilen <julianboilen@gmail.com>
2015-02-14 18:37:06 -0500

    Test that the graph is still a Graph after running through layoutengine

--- test/test-layout.js
+++ test/test-layout.js
@@ -10,17 +10,18 @@ var CardinalPortPosition = require(lib + 'model/cardinalPortPosition');
 var LayoutEngine = require(lib + 'layout/dummyLayoutV2');
 
 var generateGraph = function() {
-    // the world's simplest graph
-    var port = new Port('a+a', 'input', new CardinalPortPosition(50, 'N'));
-    var node = new Node('a', [port], []);
-    var graph = new Graph([node], []);
-    return graph;
+  // the world's simplest graph
+  var port = new Port('a+a', 'input', new CardinalPortPosition(50, 'N'));
+  var node = new Node('a', [port], []);
+  var graph = new Graph([node], []);
+  return graph;
 };
 
 
 describe('layout engine', function() {
-    it('should take a graph and not blow up', function(done) {
-      LayoutEngine(generateGraph());
-      done();
-    });
+  it('should take a graph and not blow up', function(done) {
+    var testGraph = generateGraph();
+    LayoutEngine(testGraph);
+    testGraph.should.be.an.instanceOf(Graph);
+  });
 });
