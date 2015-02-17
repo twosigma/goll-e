ed3bd6b92c4d3b1fcb86ef27fe48a6c3e3ac9f8c
Julian Boilen <julianboilen@gmail.com>
2015-02-17 15:02:57 -0500

    Move attribute out of its own folder

--- test/test-attribute.js
+++ test/test-attribute.js
@@ -1,5 +1,5 @@
 var should = require('should');
-var Attribute = require('./../lib/attribute/attribute');
+var Attribute = require('./../lib/attribute');
 var ObjectUtils = require('./../lib/utilities/objects');
 var createClass = require('./../lib/utilities/createClass');
 
