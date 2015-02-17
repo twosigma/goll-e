968f1ee3ad32242b145eb1d4ff185368e21c6d56
johnwobrien <jwo4383@rit.edu>
2015-02-17 14:37:42 -0500

    Testing updated
    added a second node and a connection

--- test/test-layout.js
+++ test/test-layout.js
@@ -6,6 +6,7 @@ var Graph = require(lib + 'model/graph');
 var Node = require(lib + 'model/node');
 var Port = require(lib + 'model/port');
 var CardinalPortPosition = require(lib + 'model/cardinalPortPosition');
+var Connection = require(lib + 'model/connection');
 
 var LayoutEngineV1 = require(lib + 'layout/dummyLayoutV1');
 var LayoutEngineV2 = require(lib + 'layout/dummyLayoutV2');
@@ -14,7 +15,11 @@ var generateGraph = function() {
   // the world's simplest graph
   var port = new Port('a+a', 'input', new CardinalPortPosition(50, 'N'));
   var node = new Node('a', [port], []);
-  var graph = new Graph([node], []);
+  var port2 = new Port('b+b', 'output', new CardinalPortPosition(50, 'S'));
+  var node2 = new Node('b', [], [port2]);
+  var conn = new Connection('connection 1', port2, port, 'junk meta data');
+  //(id, from, to, metadata) 
+  var graph = new Graph([node, node2], [conn]);
   return graph;
 };
 
