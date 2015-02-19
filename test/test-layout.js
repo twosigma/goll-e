285b71088c68dbf72187ce34fb92de5e506c6379
Julian Boilen <julianboilen@gmail.com>
2015-02-19 10:42:34 -0500

    Update layout engine to use new data models

--- test/test-layout.js
+++ test/test-layout.js
@@ -5,18 +5,20 @@ var Node = require('./../lib/model/node');
 var Port = require('./../lib/model/port');
 var CardinalPortPosition = require('./../lib/model/cardinalPortPosition');
 var Connection = require('./../lib/model/connection');
+var CardinalDirection = require('./../lib/enum/cardinalDirection');
+
 
 var DummyLayoutEngine = require('./../lib/layout/dummyLayout');
 
 var generateGraph = function() {
   // the world's simplest graph
-  var port = new Port('a+a', 'input', new CardinalPortPosition(50, 'N'));
+  var port = new Port('a+a', 'input', new CardinalPortPosition({percentage: 50, direction: CardinalDirection.NORTH}));
   var node = new Node('a', [port], []);
-  var port2 = new Port('b+b', 'output', new CardinalPortPosition(50, 'S'));
+  var port2 = new Port('b+b', 'output', new CardinalPortPosition({percentage: 50, direction: CardinalDirection.SOUTH}));
   var node2 = new Node('b', [], [port2]);
-  var conn = new Connection('connection 1', port2, port, 'junk meta data');
+  var conn = new Connection({id:'connection 1', from: port2, to: port});
   //(id, from, to, metadata) 
-  var graph = new Graph([node, node2], [conn]);
+  var graph = new Graph({nodes: [node, node2], connections:[conn]});
   return graph;
 };
 
