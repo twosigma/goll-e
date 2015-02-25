4768d7ae5ed42c7c67963748777e58429262b8b8
Robert Gilmore <Robert.Warren.Gilmore@gmail.com>
2015-02-25 15:10:12 -0500

    Added image transformation. Made background transparent. Changed quotes.

--- lib/viewcomponents/unpinButton.jsx
+++ lib/viewcomponents/unpinButton.jsx
@@ -1,14 +1,15 @@
 var React = require('react');
 
-var pinImagePath = "M19.5,37l10,0l1.07178,24.5l3.01953,0l0.908691,-24.5l10.5,0c0,0 -0.332361,-10.3673 -7.5,-10.5c0.272303,-18.12 0,-18.5 0,-18.5l2,0l0,-3.5l-15,0l0,3.5l2,0c0,0 -0.039881,0.0580001 0,18.5c-7.62955,0.0191484 -7,10.5 -7,10.5Z";
+var pinImagePath = 'M19.5,37l10,0l1.07178,24.5l3.01953,0l0.908691,-24.5l10.5,0c0,0 -0.332361,-10.3673 -7.5,-10.5c0.272303,-18.12 0,-18.5 0,-18.5l2,0l0,-3.5l-15,0l0,3.5l2,0c0,0 -0.039881,0.0580001 0,18.5c-7.62955,0.0191484 -7,10.5 -7,10.5Z';
+var pinImageTransform = 'rotate(30) scale(0.25) translate(-32, -32)';
 
 var UnpinButton = React.createClass({
 
   render: function() {
     return (
       <g className='unpinButton' {...this.props}>
-        <path d={pinImagePath} x={0} y={0} transform="rotate(30) scale(0.25)" />
-        <circle className='background' cx="0" cy="0" r="16" />
+        <circle className='background' cx='0' cy='0' r='8' />
+        <path d={pinImagePath} x={0} y={0} transform={pinImageTransform} />
       </g>
     );
   }
