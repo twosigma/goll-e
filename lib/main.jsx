cacf49b6dddfbd0baf444414641ae49764383d68
Robert Gilmore <Robert.Warren.Gilmore@gmail.com>
2015-02-26 19:52:10 -0500

    integration of stubs

--- lib/main.jsx
+++ lib/main.jsx
@@ -1,11 +1,14 @@
 var React = require('react');
 var App = require('./viewcomponents/app.jsx');
-var GraphParser = require('./model/graphParser');
+var ModelFactory = require('./parse/modelFactory');
+var Lex = require('./parse/lex');
+var SemanticAnalysis = require('./parse/semanticAnalysis');
 
-var TEST_DATA = require('./test-data.json');
+var ASTExample = require('./parse/ASTExample.json');
 
-var parser = new GraphParser();
-var graph = parser.parseGraph(TEST_DATA);
+var modelFactory = new ModelFactory();
+var graph = modelFactory.buildGraphModel(ASTExample);
+var gclErrors = [];
 
 // rerender on any change
 // graph.after('change', renderGraph);
@@ -14,7 +17,17 @@ graph.after('change', function(e) {
 });
 
 var renderGraph = function() {
-  React.render(<App graphModel={graph} />, document.getElementById('app-container'));
+  React.render(<App graphModel={graph} gclErrors={gclErrors} />, document.getElementById('app-container'));
 };
+
+// TODO: Call this function when the text in the editor changes.
+var renderGraphFromGCL = function(contentLanguageText) {
+  var abtractSyntaxTree = Lex(contentLanguageText);
+  gclErrors = SemanticAnalysis(abtractSyntaxTree);
+  if (gclErrors.length === 0) {
+    graph = modelFactory.buildGraphModel(abtractSyntaxTree);
+  }
+  renderGraph();
+}
 
 window.onload = renderGraph;
