4ea06a1d93cf979269ba1e7788e09a6d8fec8240
Julian Boilen <julianboilen@gmail.com>
2015-03-07 14:53:59 -0500

    Buttons on panels, with example on credits
    example will be removed

--- lib/viewcomponents/gui/panels/credits.jsx
+++ lib/viewcomponents/gui/panels/credits.jsx
@@ -13,6 +13,15 @@ var CreditsPanel = React.createClass({
           <li>Robert Warren Gilmore</li>
         </ul>
         <p>at the <br/><b>Rochester Institute of Technology</b></p>
+
+        <div className="button-bar">
+          <div className="wrapper">
+            {/*buttons must be divs with class button. <input> and <button> cannot be styled for a button bar */}
+            <div className="button">Button 1</div>
+            <div className="button">Button 2</div>
+            <div className="button">Button 3</div>
+          </div>
+        </div>
       </div>
     )
   }
