fb3455eb4397d940a4d37a7bafb677cbceb7e846
Robert Gilmore <Robert.Warren.Gilmore@gmail.com>
2015-03-12 22:18:03 -0400

    missing argument

--- test/test-layout.js
+++ test/test-layout.js
@@ -9,7 +9,7 @@ var parse = require('./../lib/parse/parse.js');
 var graphsDir = path.join(__dirname , 'graphs');
 
 
-var getGraph = function() {
+var getGraph = function(name) {
   var gcl = String(fs.readFileSync(path.join(graphsDir, name + '.gcl')));
   var graph = parse(gcl);
   return graph;
