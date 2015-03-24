1b2e2c77009969ba4821f3afce01517d3936923c
Julian Boilen <julianboilen@gmail.com>
2015-03-24 17:54:45 -0400

    Copy a *working* algorithm from the internets

--- lib/viewcomponents/smoothCurvedEdges.jsx
+++ lib/viewcomponents/smoothCurvedEdges.jsx
@@ -20,97 +20,63 @@ var smoothCurvedEdges = function(outputLoc, reroutePoints, inputLoc, addRerouteC
 
   var firstControlPoints = new Array(nPoints - 1);
   var secondControlPoints = new Array(nPoints - 1);
-  // var ctrlIn = new Array(nPoints);
-  // var ctrlOut = new Array(nPoints);
+  var i;
+
+
+  var controlPointsX = computeControlPoints1D(points.map(function(p) {
+    return p.x;
+  }));
+  var controlPointsY = computeControlPoints1D(points.map(function(p) {
+    return p.y;
+  }));
+
+  for (i = 0; i < nPoints - 1; i++) {
+    firstControlPoints[i] = {
+      x: controlPointsX.firstControlPoints[i],
+      y: controlPointsY.firstControlPoints[i]
+    };
+    secondControlPoints[i] = {
+      x: controlPointsX.secondControlPoints[i],
+      y: controlPointsY.secondControlPoints[i]
+    }
+  }
+
+
+  // Now we change the first and last control points so that lines
+  // come out of verticies perpendicularly. It may ruin the smoothness.
 
   // The change between the start and end.
-  // var delta = {
-  //   x: inputLoc.x - outputLoc.x,
-  //   y: inputLoc.y - outputLoc.y
-  // };
+  var delta = {
+    x: inputLoc.x - outputLoc.x,
+    y: inputLoc.y - outputLoc.y
+  };
 
   //pythagorize it
-  // delta.distance = Math.sqrt(Math.pow(delta.x, 2) + Math.pow(delta.y, 2));
+  delta.distance = Math.sqrt(Math.pow(delta.x, 2) + Math.pow(delta.y, 2));
 
   /*
   The distance from the source/target to the nearest curve control point.
   It should be some nondecreasing function of the displacement, here's one such function.
   There may be a better one.
    */
-  // var ctrlDistance = Math.min(0.3 * delta.distance, 100);
-  // var ctrlDistance = 0;
+  var ctrlDistance = Math.min(0.3 * delta.distance, 100);
 
-  // ctrlOut[0] = getPointDistanceFromPoint(ctrlDistance, outputLoc);
-  // ctrlIn[nPoints - 1] = getPointDistanceFromPoint(ctrlDistance, inputLoc);
+  firstControlPoints[0] = getPointDistanceFromPoint(ctrlDistance, outputLoc);
+  secondControlPoints[nPoints - 2] = getPointDistanceFromPoint(ctrlDistance, inputLoc);
 
-  //http://www.codeproject.com/Articles/31859/Draw-a-Smooth-Curve-through-a-Set-of-2D-Points-wit
-
-  if (nPoints == 2) {
-    firstControlPoints[0] = {
-      x: (2 * points[0].x + points[1].x) / 3,
-      y: (2 * points[0].y + points[1].y) / 3
-    };
-
-    secondControlPoints[0] = {
-      x: 2 * firstControlPoints[0].x - points[0].x,
-      y: 2 * firstControlPoints[0].y - points[0].y
-    };
-  } else {
-    // Right hand side vector
-    var rhs = new Array(nPoints - 1);
-
-    // Set right hand site X values
-    for (var i = 1; i < nPoints - 2; i++) {
-      rhs[i] = 4 * points[i].x + 2 * points[i + 1].x;
-    }
-    rhs[0] = points[0].x + 2 * points[1].x;
-    rhs[nPoints - 2] = (8 * points[nPoints - 2].x + points[nPoints - 1].x) / 2;
-    var x = getFirstControlPoints(rhs);
-
-    // Set right hand side Y values
-    for (i = 1; i < nPoints - 2; i++) {
-      rhs[i] = 4 * points[i].y + 2 * points[i + 1].y;
-    }
-    rhs[0] = points[0].y + 2 * points[1].y;
-    rhs[nPoints - 2] = (8 * points[nPoints - 2].y + points[nPoints - 1].y) / 2;
-    var y = getFirstControlPoints(rhs);
-
-    for (i = 0; i < nPoints - 1; i++) {
-      firstControlPoints[i] = {x: x[i], y: y[i]};
-
-      if (i < nPoints - 2) {
-        secondControlPoints[i] = {
-          x: 2 * points[i + 1].x - x[i + 1],
-          y: 2 * points[i + 1].y - y[i + 1]
-        };
-      } else {
-        secondControlPoints[i] = {
-          x: (points[nPoints - 1].x + x[nPoints - 2]) / 2,
-          y: (points[nPoints - 1].y + y[nPoints - 2]) / 2
-        };
-      }
-    }
-
-  }
 
 
   if (DEBUG) {
     var debugMarkers = (<g className="debug-markers">
-      <g className="reroute">{/*
-        reroutePointsPlain.map(function(p) {
-          return (<circle r="5" cy={p.y} cx={p.x} />);
-        }
-      )*/}
-      </g>
       <g className="ctrl-points">{
         firstControlPoints.map(function(p) {
           if (!p) return null;
-          return (<circle className="in" r="5" cy={p.y} cx={p.x} />);
+          return (<circle className="first" r="5" cy={p.y} cx={p.x} />);
         })
       }{
         secondControlPoints.map(function(p) {
           if (!p) return null;
-          return (<circle className="out" r="5" cy={p.y} cx={p.x} />);
+          return (<circle className="second" r="5" cy={p.y} cx={p.x} />);
         })
       }
       </g>
@@ -133,24 +99,63 @@ var smoothCurvedEdges = function(outputLoc, reroutePoints, inputLoc, addRerouteC
   return (<g>{svgPaths}{DEBUG ? debugMarkers : null}</g>);
 };
 
-var getFirstControlPoints = function(rhs) {
-  var n = rhs.length;
-  var x = new Array(n);
-  var tmp = new Array(n);
+//https://www.particleincell.com/wp-content/uploads/2012/06/bezier-spline.js
+
+var computeControlPoints1D = function(knots) {
   var i;
+  var p1 = new Array(knots.length - 1);
+  var p2 = new Array(knots.length - 2);
+  var n = knots.length - 1;
 
-  var b = 2;
-  x[0] = rhs[0] / b;
+  /*rhs vector*/
+  var a = new Array(n);
+  var b = new Array(n);
+  var c = new Array(n);
+  var r = new Array(n);
+
+  /*left most segment*/
+  a[0] = 0;
+  b[0] = 2;
+  c[0] = 1;
+  r[0] = knots[0] + 2 * knots[1];
+
+  /*internal segments*/
   for (i = 1; i < n; i++) {
-    tmp[i] = 1 / b;
-    b = (i < n - 1 ? 4 : 3.5) - tmp[i];
-    x[i] = (rhs[i] - [i - 1]) / b;
-  }
-  for (i = 1; i < n; i++) {
-    x[n - i - 1] -= tmp[n - i] * x[n - i];
+    a[i] = 1;
+    b[i] = 4;
+    c[i] = 1;
+    r[i] = 4 * knots[i] + 2 * knots[i + 1];
   }
 
-  return x;
+  /* right segment */
+  a[n - 1] = 2;
+  b[n - 1] = 7;
+  c[n - 1] = 0;
+  r[n - 1] = 8 * knots[n - 1] + knots[n];
+
+  /*solves Ax=b with the Thomas algorithm (from Wikipedia)*/
+  for (i = 1; i < n; i++) {
+    var m = a[i] / b[i - 1];
+    b[i] = b[i] -m * c[i - 1];
+    r[i] = r[i] - m * r[i - 1];
+  }
+
+  p1[n - 1] = r[n - 1] / b[n - 1];
+  for (i = n - 2; i >= 0; --i){
+    p1[i] = (r[i] - c[i] * p1[i + 1]) / b[i];
+  }
+
+  /*we have p1, now compute p2*/
+  for (i = 0; i < n - 1; i++) {
+    p2[i] = 2 * knots[i + 1] - p1[i + 1];
+  }
+
+  p2[n - 1] = 0.5 * (knots[n] + p1[n - 1]);
+
+   return {
+      firstControlPoints: p1,
+      secondControlPoints: p2
+   };
 };
 
 /*
