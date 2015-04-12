1f181a2276e74b5ca29b6930642bdb669301d704
Julian Boilen <julianboilen@gmail.com>
2015-04-12 13:49:32 -0400

    File browser component (unwired)

--- lib/viewcomponents/gui/panels/fileManager.jsx
+++ lib/viewcomponents/gui/panels/fileManager.jsx
@@ -1,24 +1,27 @@
 var React = require('react');
 var DropdownButton = require('../dropdownButton');
+var FileBrowser = require('../fileBrowser');
+var FileTypes = require('../../../enum/fileTypes');
 
 var FileManagerPanel = React.createClass({
   render: function() {
     return (
       <div className='file-manager-panel'>
-        <div className="browser"/>
+
+        <FileBrowser />
 
         <div className='button-bar'>
           <div className='wrapper'>
             <div className='button' onClick={this._promptUpload}>Upload</div>
             <DropdownButton label='New' items={[
-              {label: 'Content', onClick: this._promptUpload.bind(this, 'content')},
-              {label: 'Layout', onClick: this._promptUpload.bind(this, 'layout')},
-              {label: 'Styles', onClick: this._promptUpload.bind(this, 'style')}
+              {label: 'Content', onClick: this._promptUpload.bind(this, FileTypes.CONTENT)},
+              {label: 'Layout', onClick: this._promptUpload.bind(this, FileTypes.LAYOUT)},
+              {label: 'Styles', onClick: this._promptUpload.bind(this, FileTypes.STYLES)}
               ]}/>
             <DropdownButton label='Save As&hellip;' items={[
-              {label: 'Content', onClick: this._promptSaveAs.bind(this, 'content')},
-              {label: 'Layout', onClick: this._promptSaveAs.bind(this, 'layout')},
-              {label: 'Styles', onClick: this._promptSaveAs.bind(this, 'style')}
+              {label: 'Content', onClick: this._promptSaveAs.bind(this, FileTypes.CONTENT)},
+              {label: 'Layout', onClick: this._promptSaveAs.bind(this, FileTypes.LAYOUT)},
+              {label: 'Styles', onClick: this._promptSaveAs.bind(this, FileTypes.STYLES)}
               ]}/>
           </div>
         </div>
@@ -30,7 +33,6 @@ var FileManagerPanel = React.createClass({
   _promptUpload: function() {
     //TODO
   },
-
   _promptNew: function(type) {
     //TODO
   },
