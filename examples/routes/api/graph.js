6e7d9470610a892305b0e327374be4386456bafd
Julian Boilen <julianboilen@gmail.com>
2015-05-17 19:46:45 -0400

    Upload and delete working

--- examples/routes/api/graph.js
+++ examples/routes/api/graph.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var multiparty = require('multiparty');
 var router = express.Router();
 var path = require('path');
 var fs = require('fs');
@@ -42,7 +41,7 @@ router.get("/graphs/" + endpoint, function(req, res, next) {
   // Figure out the path to the file.
   var file = path.join(config.active_dir, req.params.gcl);
 
-  // Make sure the file exists. 
+  // Make sure the file exists.
   fs.exists(file, function(exists) {
     // If the file doesn't exist, return a 404.
     if( !exists ) {
@@ -69,48 +68,13 @@ router.get("/graphs/" + endpoint, function(req, res, next) {
 
 /* POST A new graph to the collection. */
 router.post("/graphs/", function(req, res, next) {
-  // Configure multi-part form uploads.
-  var options = {
-    autoFiles: true,
-    uploadDir: config.active_dir
-  };
-  var form = new multiparty.Form(options);
-
-  // Handle multi-part form data.
-  form.parse(req, function( err, fields, files ) {
-    // If there was some kind of error parsing the multi-part form, 
-    // forward it to our error handler and bail out.
-    if (err) {
-      next(err);
-      return;
-    }
-
-    // Make sure we actually have a filename field in whatever form is
-    // submitting the request.
-    if (!fields.hasOwnProperty("filename")) {
-      throw "No filename property.";
-    }
-    
-    // We must have at least something file-like for the gcl_content field.
-    if (!files.hasOwnProperty("gcl_content")) {
-      throw "No GCL Content";
-    }
-
-    // At this point, Multiparty will have uploaded the file to the graphs
-    // directory, but it won't be the name that we want. We'll go ahead and
-    // move the file to the proper location.
-    var temp = files.gcl_content[0].path;
-    var dest = path.join(config.active_dir, fields.filename[0]);
-    fs.rename(temp, dest, function(err) {
-      if (err) {
-        next(err);
-        return;
-      }
-    });
-  });
-
-  // If we've made it this far, then we succeeded in uploading the file.
-  res.status(200).end();  
+ console.log(req.body);
+ fs.writeFile(path.join(config.active_dir, req.body.filename), req.body.contents, function(err) {
+     if(err) {
+      res.status(500).send({error: err});
+     }
+     res.send({success: true});
+ });
 });
 
 /* PUT Update an existing graph. */
@@ -123,7 +87,7 @@ router.put("/graphs/" + endpoint, function(req, res, next) {
 /* DELETE Remove an existing graph. */
 /* Note that this doesn't actually delete anything, it moves files to the
  * archive directory */
-router.delete("/graphs/" + endpoint, function(req, res, next) {
+router.delete("/graphs/:gcl", function(req, res, next) {
   // Build up a path to the existing file, assuming it exists.
   var existing = path.join(config.active_dir, req.params.gcl);
   var destination = path.join(config.archive_dir, req.params.gcl);
@@ -138,11 +102,11 @@ router.delete("/graphs/" + endpoint, function(req, res, next) {
           next(err);
           return;
         }
-        res.status(200).end();
+        res.status(200).send({success: true});
       });
     }
     else {
-      res.status(404).end();
+      res.status(404).send({error: 'File not found'});
     }
   });
 });
--- lib/viewcomponents/gui/panels/fileManager.jsx
+++ lib/viewcomponents/gui/panels/fileManager.jsx
@@ -2,17 +2,31 @@ var React = require('react');
 var DropdownButton = require('../dropdownButton');
 var FileBrowser = require('../fileBrowser');
 var FileTypes = require('../../../enum/fileTypes');
+var storage = require('../../../files/storage-model');
 
 var FileManagerPanel = React.createClass({
+  getInitialState: function() {
+    return {files: storage.get('files')};
+  },
+
+  componentDidMount: function() {
+    storage.load();
+    storage.after('filesChange', function(e) {
+      this.setState({files: e.newVal});
+    }, this);
+  },
+
   render: function() {
     return (
       <div className='file-manager-panel'>
 
-        <FileBrowser />
+        <FileBrowser files={this.state.files} />
 
         <div className='button-bar'>
           <div className='wrapper'>
-            <div className='button' onClick={this._promptUpload}>Upload</div>
+            <div className='button' onClick={this._promptUpload}>Upload
+            <input type='file' ref='fileInput' style={{opacity: 0, left:0, position: 'absolute', cursor: 'pointer'}} onChange={this._handleUpload} />
+            </div>
             <DropdownButton label='New' items={[
               {label: 'Content', onClick: this._promptUpload.bind(this, FileTypes.CONTENT)},
               {label: 'Layout', onClick: this._promptUpload.bind(this, FileTypes.LAYOUT)},
@@ -39,6 +53,34 @@ var FileManagerPanel = React.createClass({
 
   _promptSaveAs: function(type) {
     // TODO
+  },
+
+  _handleUpload: function(e) {
+    var fileInput = this.refs.fileInput.getDOMNode();
+
+    var file = fileInput.files[0];
+    var fileName = file.name;
+
+    this._readFile(file).then(function(contents) {
+      storage.createFile(fileName, contents);
+    }.bind(this));
+  },
+
+  _readFile: function(file) {
+    return new Promise(function(resolve, reject) {
+
+      var reader = new FileReader();
+
+      reader.onload = function(e) {
+        resolve(reader.result);
+      };
+
+      reader.onerror = function(e) {
+        reject(e);
+      };
+
+      reader.readAsText(file);
+    });
   }
 });
 
